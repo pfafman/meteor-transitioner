663f48c449c828d03fac716c74df74fb3a875f42
Timothy Pfafman <tim@pfafman.com>
2018-03-31 07:16:21 -0600

    constraints

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'pfafman:transitioner',
   summary: 'Page transitions integrated with Iron Router.',
-  version: '2.0.3',
+  version: '2.0.4',
   git: 'https://github.com/pfafman/meteor-transitioner'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1');
+  //api.versionsFrom('METEOR@1');
 
   api.use([
     'iron:router',
